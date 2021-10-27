3963fdf4c706c0643d3ef3b2757ea1a79c532575
achoi98 <54051582+achoi98@users.noreply.github.com>
2021-10-26 22:38:07 -0600

    Time: 32 ms (29.26%), Space: 27.6 MB (8.94%) - LeetHub

--- max-area-of-island/max-area-of-island.cpp
+++ max-area-of-island/max-area-of-island.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int count = 0;
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<int> vec(m);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j]) {
-                    count = max(count, traverse(i, j, m, n, grid));
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<int>> dirs {{1, 0}, {-1, 0}, {0, 1}, {0,  -1}};
+        
+        int max = 0;
+        
+        queue<vector<int>> q;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] != 1) continue;
+                q.push({i, j});
+                int curr = 0;
+                grid[i][j] = 0;
+                while (!q.empty()) {
+                    curr++;
+                    vector<int> n = q.front();
+                    q.pop();
+                    int r = n[0];
+                    int c = n[1];
+                    for (int k = 0; k < dirs.size(); k++) {
+                        int nr = r + dirs[k][0];
+                        int nc = c + dirs[k][1];
+                        if (nr < 0 || nr >= rows || nc < 0 || nc >= cols || grid[nr][nc] != 1) continue;
+                        q.push({nr, nc});
+                        grid[nr][nc] = 0;
                     }
                 }
+                if (curr > max) max = curr;
             }
-        return count;
         }
-    
-    int traverse(int i, int j, int m, int n, vector<vector<int>>& grid) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1) return 0;
-        else {
-            grid[i][j] = -1;
-            return 1 + traverse(i - 1, j, m, n, grid) + traverse(i + 1, j, m, n, grid) + traverse(i, j - 1, m, n, grid) + traverse(i, j + 1, m, n, grid);
-        }
+        return max;
     }
 };
