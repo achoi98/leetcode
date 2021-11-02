d3d285bb20cf8c62b94e2ebe4dbcbb3575899e4f
achoi98 <54051582+achoi98@users.noreply.github.com>
2021-11-02 17:47:31 -0600

    Time: 4 ms (91.33%), Space: 13.7 MB (18.01%) - LeetHub

--- rotting-oranges/rotting-oranges.cpp
+++ rotting-oranges/rotting-oranges.cpp
@@ -7,23 +7,29 @@ public:
         queue<vector<int>> q;
         int fresh = 0;
         
+        // push rotten oranges onto queue, count number of fresh oranges
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
+                // rotten orange -> push coordinates to queue
                 if (grid[i][j] == 2) {
                     q.push({i, j});
                 }
+                // fresh orange -> increment count of fresh oranges
                 if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
         }
+        
+        // no fresh oranges at time 0
         if (fresh == 0) return 0;
         
-        int count = -1;
+        
+        int count = -1; // -1 because count is incremented in the very first iteration
         vector<int> dir = {1, 0, -1, 0, 1};
         
         while (!q.empty()) {
-            count++;
+            count++;    // +1 minute
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 vector<int> node = q.front();
@@ -31,11 +37,15 @@ public:
                 for (int j = 0; j < 4; j++) {
                     int row = node[0] + dir[j];
                     int col = node[1] + dir[j + 1];
+                    // proceed only if i, j are valid AND grid[i][j] is a fresh orange
                     if (col < 0 || row < 0 || col >= cols || row >= rows || grid[row][col] != 1) {
                         continue;
                     }
+                    // change grid[i][j] to 2 to indicate that it is now rotten
                     grid[row][col] = 2;
+                    // push the now rotten orange onto the queue
                     q.push({row, col});
+                    // decrease number of fresh oranges by 1
                     fresh--;
                 }
             }
