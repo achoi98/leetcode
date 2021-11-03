a8deb0fdc2b0eeec0cc132ef1fc0420cce384751
achoi98 <54051582+achoi98@users.noreply.github.com>
2021-11-02 19:03:50 -0600

    Time: 4 ms (95.43%), Space: 14.9 MB (54.07%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,45 +11,51 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (l1 == nullptr && l2 == nullptr) return nullptr;
-        if (l1 == nullptr) return l2;
-        if (l2 == nullptr) return l1;
-        ListNode* head = new ListNode();
-        ListNode* curr;
-        ListNode* node1 = l1;
-        ListNode* node2 = l2;
-        if (node1->val >= node2->val) {
-            head->val = node2->val;
-            node2 = node2->next;
-            curr = head;
+        // WLOG, merge l2 into l1
+        if (!l1) return l2;
+        if (!l2) return l1;
+        
+        ListNode* p1 = l1;
+        ListNode* p2 = l2;
+        if (p1->val < p2->val) {
+            p1 = l2;
+            p2 = l1;
         }
-        else if (node1->val < node2->val) {
-            head->val = node1->val;
-            node1 = node1->next;
-            curr = head;
-        }
-        while (node1 != nullptr && node2 != nullptr) {
-            if (node1->val >= node2->val) {
-                curr->next = new ListNode(node2->val, nullptr);
-                curr = curr->next;
-                node2 = node2->next;
+        ListNode* head = p1;
+        ListNode* prev = p1;
+        
+        while (p1 && p2) {
+            // val1 > val2 -> merge node2 into list1
+            if (p1->val >= p2->val) {
+                // p1 is the head of the list, establish prev pointer
+                if (p1 == head) {
+                    cout << "1\n";
+                    ListNode* temp = p2->next;
+                    p2->next = p1;
+                    prev = p2;
+                    head = p2;
+                    p2 = temp;
+                }
+                else {
+                    cout << "2\n";
+                    ListNode* temp = p2->next;
+                    p2->next = p1;
+                    prev->next = p2;
+                    p2 = temp;
+                    prev = prev->next;
+                }
             }
             else {
-                curr->next = new ListNode(node1->val, nullptr);
-                curr = curr->next;
-                node1 = node1->next;
+                cout << "3\n";
+                p1 = p1->next;
+                prev = prev->next;
             }
         }
-        while (node1 != nullptr) {
-            curr->next = new ListNode(node1->val, nullptr);
-            curr = curr->next;
-            node1 = node1->next;
+        
+        if (p2) {
+            prev->next = p2;
         }
-        while (node2 != nullptr) {
-            curr->next = new ListNode(node2->val, nullptr);
-            curr = curr->next;
-            node2 = node2->next;
-        }
+        
         return head;
     }
 };
